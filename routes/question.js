228faedad77990788243d9b8eece6fd43df6fb9c
Katie Breuil <kcbreuil@gmail.com>
2020-07-11 16:36:07 -0400

    added question routes

---
+++ routes/question.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = new express.Router();
+const Question = require("../models/question");
+const auth = require("../middleware/auth.js");
+
+router.get("/questions", async (req, res) => {
+  Question.find({})
+    .then((questions) => {
+      res.send(questions);
+    })
+    .catch((e) => {
+      res.send(e);
+    });
+});
+
+router.post("/questions", async (req, res) => {
+  const question = new Question({
+    ...req.body,
+  });
+  try {
+    question.save();
+    res.status(201).send(question);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+module.exports = router;
