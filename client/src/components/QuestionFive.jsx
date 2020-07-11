180086443fdac50d1f30018244055b7eb40f8ec2
Katie Breuil <kcbreuil@gmail.com>
2020-07-10 22:23:05 -0400

    work in progress

---
+++ client/src/components/QuestionFive.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const QuestionFive = () => {
+  return (
+    <div>
+      <h1>Last One!</h1>
+      <h3>
+        What famous San Francisco bread type would have been served at Google
+        Next lunches?
+      </h3>
+      <div>A. Ciabatta</div>
+      <div className="correct">B. Sourdough</div>
+      <div>C. Tortillas</div>
+      <div>D. Bagels</div>
+    </div>
+  );
+};
+
+export default QuestionFive;
---
+++ client/src/pages/Trivia.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import QuizData from "../components/QuizData";
+
+const Trivia = () => {
+  //   const [userAnswer, setUserAnswer] = useState(null);
+  //   const [currentIndex, setCurrentIndex] = useState(0);
+  //   const [options, setOptions] = useState([]);
+  //   const [quizEnd, setQuizEnd] = useState(false);
+  //   const [score, setScore] = useState(0);
+  //   const [disabled, setDisabled] = useState(true);
+
+  //   const loadQuiz = () => {
+  //     const { currentIndex } = setCurrentIndex[0];
+  //   };
+
+  return (
+    <div>
+      <QuizData />
+    </div>
+  );
+};
+
+export default Trivia;
