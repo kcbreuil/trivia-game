6d6799c07b40b05dd9db51ec6c2e1acd2098ade3
marcellits <garcia.marcella@hotmail.com>
2020-07-16 16:22:05 -0400

    Functionality to spin the wheel added

--- client/src/components/Wheel.jsx
+++ client/src/components/Wheel.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React from "react"; 
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+import Wheel_function from "../utils/wheel";
 
-const Wheel = () => {
-  return <div>this is where the wheel would be spun</div>;
-};
+export class Wheel extends React.Component {
+  constructor() {
+    super();
+    this.places = [":(", "10","50", "100", "150"];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Spin to win! </h1>
+        <Wheel_function items={this.places} />
+      </div>
+    );
+  }
+}
 
 export default Wheel;
+
