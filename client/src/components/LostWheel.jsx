babb6331643fd3831cb1a4699cd3c61881d1db31
Katie Breuil <kcbreuil@gmail.com>
2020-07-20 23:19:12 -0400

    more styling

--- client/src/components/LostWheel.jsx
+++ client/src/components/LostWheel.jsx
@@ -3,17 +3,59 @@ import React from "react";
 const LostWheel = () => {
   return (
     <div style={{ textAlign: "center" }}>
+      <div style={{ marginBottom: "25px" }}>
+        <h1 style={{ textAlign: "center", fontSize: "42px" }}>
+          <span style={{ fontWeight: "lighter" }}>Next</span>&nbsp;Tech Trivia
+          <br></br>
+          <span style={{ fontSize: "27px", fontWeight: "lighter" }}>with</span>
+          &nbsp;
+          <span style={{ color: "#0072bc", font: "bold", fontSize: "42px" }}>
+            NetApp
+          </span>
+        </h1>
+      </div>
       <h1>Awe Snap!</h1>
       <img
         src="https://res.cloudinary.com/farmersmarket/image/upload/v1595185391/Asset_77_usmr2y.png"
         alt=""
         style={{ height: "100px" }}
       />
-      <h4>
-        {" "}
-        Sorry you didn’t win today, but your name will still be entered into our
-        grand prize drawing. Thanks for playing!
-      </h4>
+      <div style={{ marginBottom: "100px" }}>
+        <p>
+          Sorry you didn’t win today, but your name will still be entered into
+          our grand prize drawing.
+          <p>
+            <b>Thanks for playing!</b>
+          </p>
+          <br></br>
+          <p>
+            <b>Now, let’s get back to Google Cloud Next ’20: On Air.</b>
+          </p>
+          <i>
+            Please read and understand the NetApp Privacy Policy and Trivia
+            Terms and Conditions, and understand that you can unsubscribe from
+            NetApp communications at any time or manage your preferences.
+          </i>
+        </p>
+      </div>
+      <button
+        style={{
+          border: "none",
+          cursor: "pointer",
+          color: "white",
+          backgroundColor: "#f1bd42",
+          padding: "10px",
+          borderRadius: "20px",
+          display: "inline-block",
+          marginRight: "15px",
+          marginTop: "5vh",
+          marginBottom: "5vh",
+          width: "100px",
+          fontWeight: "bold",
+        }}
+      >
+        CLOSE {"X"}
+      </button>
     </div>
   );
 };
--- client/src/components/WonWheel.jsx
+++ client/src/components/WonWheel.jsx
@@ -2,25 +2,45 @@ import React from "react";
 
 function WonWheel() {
   return (
-    <div>
+    <div className="winning-background">
       <div>
-        <h1 style={{ textAlign: "center" }}>
-          Next Tech Trivia <br></br>
-          <span style={{ fontSize: "16px", fontWeight: "lighter" }}>with</span>
+        <h1 style={{ textAlign: "center", fontSize: "42px" }}>
+          <span style={{ fontWeight: "lighter" }}>Next</span>&nbsp;Tech Trivia
+          <br></br>
+          <span style={{ fontSize: "27px", fontWeight: "lighter" }}>with</span>
           &nbsp;
-          <span style={{ color: "#0072bc", font: "bold" }}>NetApp</span>
+          <span style={{ color: "#0072bc", font: "bold", fontSize: "42px" }}>
+            NetApp
+          </span>
         </h1>
       </div>
-      <div>
+      <div style={{ textAlign: "center" }}>
         <h1>CONGRATULATIONS!!!</h1>
         <h2>
-          You have won a ${"insert amount won"} gift card to buy a coveted
-          Google item of your choice.
+          You have won a ${"amount"} gift card to buy a coveted Google item of
+          your choice.
         </h2>
         <p>Check your inbox for an email with your gift card information.</p>
         <h3>Thanks for playing!</h3>
         <h3>Now, let’s get back to Google Next OnAir.</h3>
-        <button>Close</button>
+        <button
+          style={{
+            border: "none",
+            cursor: "pointer",
+            color: "white",
+            backgroundColor: "#f1bd42",
+            padding: "10px",
+            borderRadius: "20px",
+            display: "inline-block",
+            marginRight: "15px",
+            marginTop: "5vh",
+            marginBottom: "5vh",
+            width: "100px",
+            fontWeight: "bold",
+          }}
+        >
+          Close
+        </button>
       </div>
     </div>
   );
