07d08ac533b7a353ebf445309a91b9c31ad2c8b0
marcellits <garcia.marcella@hotmail.com>
2020-07-24 18:14:09 -0400

    Won Wheel component - Responsive CSS

--- client/src/components/LostTrivia.jsx
+++ client/src/components/LostTrivia.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import "../styling/losttrivia.css";
+import "../styling/app.css";
 import axios from "axios";
 
 const LostTrivia = () => {
--- client/src/components/WonWheel.jsx
+++ client/src/components/WonWheel.jsx
@@ -1,5 +1,8 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import "../styling/winning.css";
+import "../styling/app.css";
+
 
 function WonWheel() {
   const { result } = useContext(AppContext);
@@ -7,58 +10,35 @@ function WonWheel() {
   return (
     <div className="winning-background">
       <div>
-        <h1 style={{ textAlign: "center", fontSize: "55px" }}>
-          <span style={{ fontWeight: "lighter" }}>Next</span>&nbsp;Tech Trivia
+        <h1 className="tech-trivia-name">
+          <span className="next-name">Next</span>&nbsp;Tech Trivia
           <br></br>
-          <span style={{ fontSize: "33px", fontWeight: "lighter" }}>with</span>
+          <span className="with-name">with</span>
           &nbsp;
-          <span
-            style={{
-              color: "#0072bc",
-              font: "bold",
-              fontSize: "55px",
-              lineHeight: "6rem",
-            }}
-          >
+          <span className="netapp-name">
             NetApp
           </span>
         </h1>
       </div>
       <div style={{ textAlign: "center" }}>
-        <h1 style={{ fontSize: "52px", fontWeight: "800" }}>
+        <h1 id="congrats">
           CONGRATULATIONS!!!
         </h1>
-        <h2 style={{ fontSize: "30px" }}>
+        <h2 className="h2-wontrivia">
           You have won a{" "}
           <span style={{ color: "#0072bc" }}>${prize[result]}</span> gift card
           to buy a coveted Google item of your choice.
         </h2>
-        <p style={{ fontSize: "25px" }}>
+        <p className="won-trivia-text">
           Check your inbox for an email with your gift card information.
         </p>
-        <p style={{ fontSize: "24px", fontWeight: "800" }}>
+        <h2 className="h2-wontrivia">
           Thanks for playing!
           <br></br>
           <br></br>
-          Now, let’s get back to Google Cloud Next '20 OnAir.
-        </p>
-        <button
-          style={{
-            border: "none",
-            cursor: "pointer",
-            color: "white",
-            backgroundColor: "#f1bd42",
-            padding: "10px",
-            borderRadius: "20px",
-            display: "inline-block",
-            marginRight: "15px",
-            marginTop: "5vh",
-            marginBottom: "5vh",
-            width: "113px",
-            fontWeight: "bold",
-            fontSize: "20px",
-          }}
-        >
+          Now, let’s get back to Google Cloud Next '20: OnAir.
+        </h2>
+        <button id="button-wonwheel">
           CLOSE {"X"}
         </button>
       </div>
