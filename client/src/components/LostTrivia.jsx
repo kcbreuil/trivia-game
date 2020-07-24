41d4e6bf9f7ac6955110154e2a16ba36008ac4da
marcellits <garcia.marcella@hotmail.com>
2020-07-24 17:13:52 -0400

    Lost Trivia component - Responsive css and api call (loser email) added

--- client/src/components/LostTrivia.jsx
+++ client/src/components/LostTrivia.jsx
@@ -1,68 +1,57 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import "../styling/winning.css";
+import "../styling/losttrivia.css";
+import axios from "axios";
 
 const LostTrivia = () => {
   const { totalCorrectAnswers } = useContext(AppContext);
-
+  
+  const sendEmail = async () => {
+    await axios.post(
+      "/sendemail",
+      { data: { data: "it doesn't matter but don't erase me" } },
+      {
+        headers: {
+          authorization: localStorage.getItem("token"),
+        },
+      }
+    );
+  }
+  sendEmail();
+  
   return (
     <div className="lost-trivia">
       <div>
-        <h1 style={{ textAlign: "center", fontSize: "55px" }}>
-          <span style={{ fontWeight: "lighter" }}>Next</span>&nbsp;Tech Trivia
+        <h1 className="tech-trivia-name" >
+          <span className="next-name">Next</span>&nbsp;Tech Trivia
           <br></br>
-          <span style={{ fontSize: "33px", fontWeight: "lighter" }}>with</span>
+          <span className="with-name" >with</span>
           &nbsp;
-          <span
-            style={{
-              color: "#0072bc",
-              font: "bold",
-              fontSize: "55px",
-              lineHeight: "6rem",
-            }}
-          >
-            NetApp
+          <span className="netapp-name">NetApp
           </span>
         </h1>
       </div>
       <div style={{ textAlign: "center" }}>
-        <h2 style={{ fontWeight: "800", marginTop: "9vh", fontSize: "30px" }}>
+        <h2 className="h2-losttrivia">
           You answered{" "}
           <span style={{ color: "#0072bc" }}>{totalCorrectAnswers}</span> out of
           5 questions correctly.
         </h2>
-        <p
-          style={{ lineHeight: "4rem", fontSize: "25px", margin: "auto 18vw" }}
+        <p className="lost-trivia-text"
         >
           You need 4 or more correct answers in order to "spin to win" a gift
           card, but you're still in the running for our grand prize drawing.
         </p>
-        <h2 style={{ fontWeight: "800", margin: "4vh auto", fontSize: "30px" }}>
+        <h2 className="h2-losttrivia">
           Thanks for playing!
         </h2>
-        <h2 style={{ fontWeight: "800", fontSize: "30px", margin: "4vh auto" }}>
-          Now, let's get back to Google Cloud Next '20 OnAir.
+        <h2 className="h2-losttrivia">
+          Now, let's get back to Google Cloud Next '20: OnAir.
         </h2>
       </div>
-
-      <div className="button-flex">
+      <div >
         <a href="http://google.com">
-          <input
-            style={{
-              border: "none",
-              cursor: "pointer",
-              color: "white",
-              backgroundColor: "#f1bd42",
-              padding: "10px",
-              borderRadius: "20px",
-              display: "inline-block",
-              marginRight: "15px",
-              marginTop: "5vh",
-              marginBottom: "5vh",
-              width: "100px",
-              fontWeight: "bold",
-              textAlign: "center",
-            }}
+          <input id="lost-trivia-button"
             type="button"
             value="CLOSE X"
           />
