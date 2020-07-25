7f7e4f3e5e3812841dec735aaecceefccbe28f3c
Katie Breuil <kcbreuil@gmail.com>
2020-07-25 19:18:24 -0400

    debug one

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,10 @@ import Page from "./pages/Page";
 
 function App() {
   return (
-    // <Router>
     <AppContextProvider>
       <Page />
-      {/* <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/trivia" component={Trivia} />
-          <Route exact path="/wheel" component={Wheel} />
-          <Route exact path="/winning" component={WonWheel} />
-          <Route exact path="/losing" component={LostTrivia} />
-          <Route exact path="/lostwheel" component={LostWheel} />
-      </Switch> */}
       <Footer />
     </AppContextProvider>
-    // </Router>
   );
 }
 
