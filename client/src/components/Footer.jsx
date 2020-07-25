d4abf4a06df40fb286f1de15197f65b22ee7b83e
Nestor <nhrivera98@gmail.com>
2020-07-24 20:14:32 -0400

    responsiveness

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,12 +5,11 @@ const Footer = () => {
   return (
     <div>
       <div className="footer">
-        <div style={{ display: "inline-grid" }}>
+        <div className="footer-grid">
           <img
             src="https://res.cloudinary.com/farmersmarket/image/upload/v1595185287/Asset_63_id5hkc.png"
             alt="net-app-logo"
-            className="footer-img"
-            style={{ height: "25px", margin: "auto", paddingLeft: "20px" }}
+            className="footer-img netApp"
           />
         </div>
         <div>
