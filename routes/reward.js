21cfea17513630068093f08db162d37282d77d11
Katie Breuil <kcbreuil@gmail.com>
2020-07-25 17:40:59 -0400

    figuring out the bug

--- routes/reward.js
+++ routes/reward.js
@@ -2,30 +2,29 @@ const express = require("express");
 const Reward = require("../models/reward");
 const router = new express.Router();
 
-router.post("/reward", async (req, res) => {
-  const reward = new Reward({ ...req.body });
-  try {
-    await reward.save();
-    console.log("aqui?");
-    res.status(201).send({ reward });
-    console.log("porque");
+// router.post("/reward", async (req, res) => {
+//   const reward = new Reward({ ...req.body });
+//   try {
+//     await reward.save();
+//     console.log("aqui?");
+//     res.status(201).send({ reward });
+//     console.log("porque");
 
-  } catch (e) {
-    console.log("or here?");
-    res.status(400).send(e);
-  }
-});
-//update rewards
+//   } catch (e) {
+//     console.log("or here?");
+//     res.status(400).send(e);
+//   }
+// });
+// //update rewards
 
-router.get("/reward", async (req, res) => {
-  Reward.find({})
-    .then((reward) => {
-      res.send(reward);
-    })
-    .catch((e) => {
-      res.send(e);
-    });
-});
-
+// router.get("/reward", async (req, res) => {
+//   Reward.find({})
+//     .then((reward) => {
+//       res.send(reward);
+//     })
+//     .catch((e) => {
+//       res.send(e);
+//     });
+// });
 
 module.exports = router;
